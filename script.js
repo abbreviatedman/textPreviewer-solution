72415bf2477ff7f60be8b8300e35cc35668d4edd
Colin Jaffe <balloonasaurus@gmail.com>
2021-10-13 13:23:29 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,213 @@
+const fonts = [
+  "Arial",
+  "Arial Black",
+  "Verdana",
+  "Tahoma",
+  "Trebuchet MS",
+  "Impact",
+  "Times New Roman",
+  "Didot",
+  "Georgia",
+  "American Typewriter",
+  "Andalé Mono",
+  "Courier",
+  "Lucida Console",
+  "Monaco",
+  "Bradley Hand",
+  "Brush Script MT",
+  "Luminari",
+  "Comic Sans MS",
+];
+
+const colors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGrey",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkSlateGrey",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "RebeccaPurple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "SlateGrey",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen",
+];
+
+// Add Javascript Here
+
+const fontSelection = document.querySelector("#font-selection");
+const colorSelection = document.querySelector("#color-selection");
+const preview = document.querySelector("#preview-placeholder");
+
+fonts.forEach((font) => {
+  const option = document.createElement("option");
+  option.value = font;
+  option.textContent = font;
+  fontSelection.append(option);
+});
+
+colors.forEach((color) => {
+  const option = document.createElement("option");
+  option.value = color;
+  option.textContent = color;
+  colorSelection.append(option);
+});
+
+document.querySelector("#text-input").addEventListener("input", (event) => {
+  preview.textContent = event.target.value;
+});
+
+colorSelection.addEventListener("change", () => {
+  preview.style.color = colorSelection.value;
+});
+
+fontSelection.addEventListener("change", () => {
+  preview.style.fontFamily = fontSelection.value;
+});
+
+document.querySelector("#save-button").addEventListener("click", () => {
+  const color = colorSelection.value;
+  const font = fontSelection.value;
+  const savedTextItem = document.createElement("li");
+  savedTextItem.textContent = preview.textContent;
+  savedTextItem.style.color = color;
+  savedTextItem.style.fontFamily = font;
+  document.querySelector("ul").append(savedTextItem);
+});
